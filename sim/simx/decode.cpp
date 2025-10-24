30a69b76b83b2b139d546a898f323618ef02b93e
Anmol Gupta <anmolgupta23@ucla.edu>
2025-10-23 17:34:49 -0700

    complete step 3

--- sim/simx/decode.cpp
+++ sim/simx/decode.cpp
@@ -112,15 +112,9 @@ static op_string_t op_string(const Instr &instr) {
         }
       }
       case AluType::CZERO: return {aluArgs.imm ? "CZERO.NEZ":"CZERO.EQZ", ""};
+      case AluType::DOT8: return {"DOT8", ""};
       default:
         std::abort();
-      }
-      case AluType::DOT8: {
-        if (aluArgs.is_imm) {
-          return {"DOT8I", to_hex_str(aluArgs.imm)};
-        } else {
-          return {"DOT8", ""};
-        }
       }
     },
     [&](VoteType vote_type)-> op_string_t {
@@ -1125,6 +1119,21 @@ void Emulator::decode(uint32_t code, uint32_t wid, uint64_t uuid) {
       }
     } break;
   #endif
+    case 3: {
+      switch (funct3) {
+        case 0: { // DOT8
+          auto instr = std::allocate_shared<Instr>(instr_pool_, uuid, FUType::ALU);
+          instr->setOpType(AluType::DOT8);
+          instr->setArgs(IntrAluArgs{0, 0, 0});
+          instr->setDestReg(rd, RegType::Integer);
+          instr->setSrcReg(0, rs1, RegType::Integer);
+          instr->setSrcReg(1, rs2, RegType::Integer);
+          ibuffer.push_back(instr);
+        } break;
+        default:
+          std::abort();
+      }
+    } break;
     default:
       std::abort();
     }
